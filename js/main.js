0ec51236e39c2c8b41ded0152fc73e71da9e5691
macarena baltra <macarena.bc2011@gmail.com>
2017-07-02 15:39:43 -0400

    Primer intento Tragedia de validación

--- js/main.js
+++ js/main.js
@@ -1,3 +1,49 @@
 function validateForm(){
-	/* Escribe tú código aquí */
+	var expre = /^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$/;
+	var expreNom = /[a-z]/;
+
+	$(document).ready(function(){
+		$('#boton').click(function(){
+			var nombre = $('#name').val();
+			var apellido = $('#lastname').val();
+			var correo = $('#input-email').val();
+			var clave = $('#input-password').val();
+			/* Ahora Validaremos */
+
+			// Nombre
+			if($('#name').val() == "" || $('#name').val() != expreNom.test($('#name').val())){
+				$('#mensajeError1').fadeIn(); // fadeIn() nos dara una animacion de opacidades, es solo para que se muestre el mensaje mas bonito
+				return false;
+			}else{
+				$('#mensajeError1').fadeOut(); // fadeOut() oculta la animación.
+			}
+
+			// Apellido
+			if($('#lastname').val() == "" || $('#lastname').val() != expreNom.test($('#lastname').val())){
+				$('#mensajeError2').fadeIn(); // fadeIn() nos dara una animacion de opacidades, es solo para que se muestre el mensaje mas bonito
+				return false;
+			}else{
+				$('#mensajeError2').fadeOut(); // fadeOut() oculta la animación.
+
+			}
+
+			// Correo
+			if($('#input-email').val() == "" || $('#input-email').val() != expre.test($('#input-email').val())){
+				$('#mensajeError3').fadeIn(); // fadeIn() nos dara una animacion de opacidades, es solo para que se muestre el mensaje mas bonito
+				return false;
+			}else{
+				$('#mensajeError3').fadeOut(); // fadeOut() oculta la animación.
+			}
+
+			// Clave
+			if($('#input-password').val() == "" || $('#input-password').val() > 6 || $('#input-password').val() == "123456" || $('#input-password').val() == "098765"){
+				$('#mensajeError4').fadeIn(); // fadeIn() nos dara una animacion de opacidades, es solo para que se muestre el mensaje mas bonito
+				return false;
+			}else{
+				$('#mensajeError4').fadeOut(); // fadeOut() oculta la animación.
+			}
+	})
 }
+
+
+// NO FUNCIONA T.T
